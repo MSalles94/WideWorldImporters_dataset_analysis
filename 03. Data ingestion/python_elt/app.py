767bb0f2dc9744e8444dce1da297c57404a591d3
Matheus Cordeiro Salles <97437324+MSalles94@users.noreply.github.com>
2026-06-16 03:14:55 +0000

    data ingestion success testing

--- 03. Data ingestion/python_elt/app.py
+++ 03. Data ingestion/python_elt/app.py
@@ -1,8 +1,14 @@
 def app():
+    with open("./output/arquivo.txt", "w") as f:
+        f.write("Olá mundo!")
+    print('txt saved')
     print('------------------------------')
     from pandas import DataFrame
-    print(DataFrame({"Test":[1,2,3],
-                     "steps":['docker','uv','python']}))
+    df_teste=DataFrame({"Test":[1,2,3],
+                     "steps":['docker','uv','python']})
+    df_teste.to_csv('./output/teste_file.csv',sep=';')
+    print('dataframe saved')
+    print(df_teste)
     print('------------------------------')
     print('Good Job!!!!')
     print('------------------------------')
