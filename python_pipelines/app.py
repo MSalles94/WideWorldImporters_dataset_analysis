7ecacc3f448711b97e74cb5ec2b8f6a17d5a361e
Matheus S <matheus.salles_1994@hotmail.com>
2026-07-11 00:44:40 -0300

    organize py

---
+++ python_pipelines/app.py
@@ -0,0 +1,11 @@
+def main():
+    import pandas as pd
+
+    print("---------------------------------------------------")
+    print("---------------------------------------------------")
+    print("Hello from pipelines!")
+    pd.DataFrame().to_csv('/app/output/teste.csv')
+
+
+if __name__ == "__main__":
+    main()
