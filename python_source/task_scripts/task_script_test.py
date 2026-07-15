504bfe467eb035e7b3ccfa83772fd0018a7dd565
Matheus Cordeiro Salles <97437324+MSalles94@users.noreply.github.com>
2026-07-15 19:04:41 +0000

    changes

---
+++ python_source/task_scripts/task_script_test.py
@@ -0,0 +1,12 @@
+from python_source.modules import utils
+
+def main ():
+    print(f"""
+    ----------------------------------------------
+    Testing python container.
+    parameter importation:{utils.parameter}
+    ----------------------------------------------
+    """)
+    pass
+if __name__ == "__main__":
+    main()
